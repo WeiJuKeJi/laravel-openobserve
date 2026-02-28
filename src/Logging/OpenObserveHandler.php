1250ea646c92a1b830ba7c1c61c5d002ca7287e0
Ruihua <ruihuachen@qq.com>
2026-02-28 17:59:07 +0800

    feat: flush immediately on error level and above

--- src/Logging/OpenObserveHandler.php
+++ src/Logging/OpenObserveHandler.php
@@ -51,6 +51,11 @@ class OpenObserveHandler extends AbstractProcessingHandler
         }
 
         $this->client->addToBatch($data);
+
+        // Flush immediately for error and above to ensure critical logs are not lost
+        if ($record->level->value >= Level::Error->value) {
+            $this->client->flush();
+        }
     }
 
     /**
